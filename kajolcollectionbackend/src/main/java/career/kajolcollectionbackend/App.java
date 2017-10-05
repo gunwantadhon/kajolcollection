4ec7db616457cfa6a281929366d1f99007c89aa4
gunwantadhon <gunwantadhon@gmail.com>
2017-10-05 22:59:52 +0530

    part 1 done by gunjan

---
+++ kajolcollectionbackend/src/main/java/career/kajolcollectionbackend/App.java
@@ -0,0 +1,13 @@
+package career.kajolcollectionbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
